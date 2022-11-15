98f55d62997fcf058916b6c9138167a32f7bcd10
Dean Ruina <druina@c2r5p9.hive.fi>
2022-11-15 14:33:44 +0200

    Working but needs nore fixes

--- get_next_line_test.c
+++ get_next_line_test.c
@@ -6,7 +6,7 @@
 /*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 16:09:34 by druina            #+#    #+#             */
-/*   Updated: 2022/11/14 16:59:20 by druina           ###   ########.fr       */
+/*   Updated: 2022/11/15 11:27:41 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,61 +14,53 @@
 
 char	*get_next_line_test(int fd)
 {
-	 char	buf[BUFFER_SIZE +1];
-	static char *temp;
-	char *line;
-	int ret;
-	char *answer;
+	char		buf[BUFFER_SIZE + 1];
+	static char	*temp;
+	char		*nline;
+	int			ret;
+	char		*ans;
+	//char		*clean;
 
 	if (fd == -1)
 		return (NULL);
-
-	if(!temp)
-		temp = (char*)malloc(sizeof(char) * 1);
 	if (!temp)
-		return(NULL);
-	while(1)
+		temp = (char *)malloc(sizeof(char) * 1);
+	if (!temp)
+		return (NULL);
+	while (1)
 	{
-
-	ret = read(fd, buf, BUFFER_SIZE);
-	if (ret < BUFFER_SIZE)
-	{
+		ret = read(fd, buf, BUFFER_SIZE);
+		if (ret == 0 && (temp == NULL || *temp == '\0'))
+			return (NULL);
 		buf[ret] = '\0';
-
-		break;
+	//	clean = temp;
+		//free(temp);
+		temp = ft_strjoin(temp, buf);
+		nline = ft_strchr(temp, '\n');
+		if (nline != NULL)
+		{
+			ans = ft_substr(temp, 0, (ft_strlen(temp) - ft_strlen(nline)) + 1);
+			temp = nline + 1;
+			return (ans);
+		}
 	}
-	else
-		buf[ret] = '\0';
-	temp = ft_strjoin(temp, buf);
-
-	line = ft_strchr(temp, '\n');
-	if (line != NULL)
-	{
-		answer = ft_substr(temp, 0, ft_strlen(temp) - ft_strlen(line));
-		temp = line +1;
-		return(answer);
-	}
-	free(temp);
-	free(line);
-	//printf("%s\n", line);
-	}
-	//printf(" this is the end: %s\n", temp);
 	return (NULL);
-
 }
 
 int	main(void)
 {
-	char *str;
-	char *str2;
-	int	fd;
-	fd = open("test.txt", O_RDWR);
-	str = get_next_line_test(fd);
-	printf("%s\n", str);
-	//free(str);
-	str2 = get_next_line_test(fd);
-	printf("%s\n", str2);
-	//free(str2);
+	char	*str;
+	int		fd;
+
+	fd = open("bible.txt", O_RDWR);
+	while (1)
+	{
+		str = get_next_line_test(fd);
+		if (str == NULL)
+			break ;
+		printf("%s", str);
+		free(str);
+	}
 	close(fd);
-		return (0);
+	return (0);
 }
